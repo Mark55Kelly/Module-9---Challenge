a4d6de3816433285feab61f5749b5d0128f59c70
Mark55Kelly <108310217+Mark55Kelly@users.noreply.github.com>
2022-09-08 19:58:43 -0500

    Updated JavaScript code for generateMD
    Adding new function GenerateMD with MD style instead of the Html document getting created. 
    Added a new utility into the system to write file easier using promisify
    Updated function to run when called.

--- index.js
+++ index.js
@@ -1,121 +1,108 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // fs.readFile('README.md', 'utf8', (error, data) => {
 //     error ? console.error(error) : console.log(data)
 // };
 
 // TODO: Create an array of questions for user input
-inquirer 
-    .prompt([
-    {
-        type: 'input',
-        message: 'What is your project title?',
-        name: 'title',
+let init = () =>
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your project title?',
+                name: 'title',
 
-    },
-    {
-        type: 'input',
-        message: 'Please enter your project description.',
-        name: 'description'
-    },
-    {
-        
-        type: 'input',
-        message: 'Please enter installation instructions.',
-        name: 'installation',
+            },
+            {
+                type: 'input',
+                message: 'Please enter your project description.',
+                name: 'description'
+            },
+            {
 
-    },
-    {
-        
-        type: 'input',
-        message: 'Please enter usage information.',
-        name: 'usage',
+                type: 'input',
+                message: 'Please enter installation instructions.',
+                name: 'installation',
 
-    },
-    {
-        
-        type: 'input',
-        message: 'Please describe any contribution guidelines.',
-        name: 'contribution',
+            },
+            {
 
-    },
+                type: 'input',
+                message: 'Please enter usage information.',
+                name: 'usage',
 
-    {
-        
-        type: 'input',
-        message: 'Please describe any test instructions',
-        name: 'instructions',
+            },
+            {
 
-    },
-    {
-        type: 'list',
-        message: 'Please choose a license for the application',
-        choices: ["Tier 1", "Tier 2", "Tier 3"],
-        name: 'license'
-    },
-    {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'gitHub',
+                type: 'input',
+                message: 'Please describe any contribution guidelines.',
+                name: 'contribution',
 
-    },
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
+            },
 
-    },
-])
-    .then((answers => {
-        const html = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <nav>${answers.license}</nav>
-            <Main>
-                <h1>${answers.title}</h1>
-                <h3>Description</h3>
-                    <P>${answers.description}</P>
-                <h3>Installation Instructions</h3>
-                    <P>${answers.installation}</P>
-                <h3>Usage Information</h3>
-                    <P>${answers.usage}</P>
-                <h3>Contributing Guidelines</h3>
-                    <P>${answers.contribution}</P>
-                <h3>Test Instructions</h3>
-                    <P>${answers.instructions}</P>
-                <h3>Questions</h3>
-                <li>
-                    <ul>${answers.gitHub}<a href="https://github.com/Mark55Kelly">Mark Kelly's Github Profile,</a></ul>
-                    <ul>If you have any additional questions, please don't hesitate to reach out at: ${answers.email}</ul>
-                </li>
-                <h4></h4>
-                <p></p>
-            </Main>
-        </body>
-        </html>`
-        fs.writeFile('index.html', html, err => err ? console.log(err) : console.log("Successful!"))
-//console.log(`Success!`)
-    }));
+            {
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.appendFile("readme.md", `${html}`, (err) => err ? console.error(err) : console.log('Success!'))
+                type: 'input',
+                message: 'Please describe any test instructions',
+                name: 'instructions',
 
-};
+            },
+            {
+                type: 'list',
+                message: 'Please choose a license for the application',
+                choices: ["Tier 1", "Tier 2", "Tier 3"],
+                name: 'license'
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'gitHub',
 
-// TODO: Create a function to initialize app
-function init() {
-    writeToFile();
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
 
+            },
+        ])
+
+function generateMD(answers) {
+    return `# 
+        ${answers.title}
+        ${answers.description}
+        ## Table of Contents: 
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [License](#license)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [Questions](#questions)
+        ### Installation: 
+        In order to install the necessary dependencies, open the console and run the following: 
+        ${answers.installations}
+        ### Usage:
+        ${answers.usage}
+        ### License:
+        This project is licensed under:
+        ${answers.license}
+        ### Contributing:
+        ${answers.contribution}
+        ### Tests:
+        In order to test open the console and run the following:
+        ${answers.tests}
+        ### Questions:
+        If you have any questions contact me on [GitHub](https://github.com/${answers.gitHub}) or you may contact me at ${answers.email}
+        `
 }
 
-// Function call to initialize app
-init();
+init()
+    .then((answers) => writeFileAsync('generateREADME.md', generateMD(answers)))
+    .then(() => console.log('Success!')
+        .catch((err) => console.error(err)));
